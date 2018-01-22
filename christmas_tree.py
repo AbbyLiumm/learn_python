57718c10559d9f401a50d154cddc7b2676a21b32
AbbyLiumm <AbbyLiu.me@gmail.com>
2018-01-22 21:19:51 +0800

    rename

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,3 +1,5 @@
+is_debug_mode = False
+
 def christmas_tree(l):
     if l <= 0:
         print("Please enter the correct height of the Christmas Tree!")
@@ -12,4 +14,10 @@ def christmas_tree(l):
         for m in range(int(l / 2) + 1):
             print(" " * (l-1) + "*")
 
-christmas_tree(4)
+num = 0
+if not is_debug_mode:
+    word = input("provide a number of the Christmas_Tree: ")
+    num = int(word)
+else:
+    num = 5
+christmas_tree(num)
