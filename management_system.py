887ca174fcde3558b26228e2407f2700c050eeed
Peiyun Liu (MSR Student-FATalent) <v-peiyli@microsoft.com>
2018-01-23 18:58:07 +0800

    easy Management

---
+++ management_system.py
@@ -0,0 +1,12 @@
+Lewei = {}
+
+name = input("请输入用户姓名：")
+age = input("请输入用户年龄：")
+gender = input("请输入用户性别：")
+position = input("请输入用户职位：")
+Lewei['name'] = name
+Lewei['age'] = age
+Lewei['gender'] = gender
+Lewei['position'] = position
+
+print(Lewei)
