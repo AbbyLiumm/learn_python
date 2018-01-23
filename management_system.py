67b60d07524db5434776e1b5047524ffe95197f0
AbbyLiumm <AbbyLiu.me@gmail.com>
2018-01-23 19:36:01 +0800

    modify management

--- management_system.py
+++ management_system.py
@@ -1,12 +1,20 @@
-Lewei = {}
+data = {}
 
-name = input("请输入用户姓名：")
-age = input("请输入用户年龄：")
-gender = input("请输入用户性别：")
-position = input("请输入用户职位：")
-Lewei['name'] = name
-Lewei['age'] = age
-Lewei['gender'] = gender
-Lewei['position'] = position
+for i in range(3):
+    alias = input("please input alias: ")
 
-print(Lewei)
+    data[alias] = {}
+    Lewei = data[alias]
+
+    name = input("please input name: ")
+    age = input("please input age: ")
+    gender = input("please input gender: ")
+    position = input("please input position: ")
+    Lewei['name'] = name
+    Lewei['age'] = age
+    Lewei['gender'] = gender
+    Lewei['position'] = position
+
+    print(Lewei)
+
+print(data)
