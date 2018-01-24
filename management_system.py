135bc9ac6f67748596a24e970029e8df28457d61
AbbyLiumm <AbbyLiu.me@gmail.com>
2018-01-24 20:25:09 +0800

    final version

--- management_system.py
+++ management_system.py
@@ -2,13 +2,16 @@ import pprint
 
 data = {}
 
-def new_file_name():                                                                                    #newfile or openfile
+def new_file_name(): # newfile or openfile
     print("please input the file name you want to create or open (!!Note the suffix name of the file!!): ")
     i = input()
     return i
 
-def read_file_data():                                                                                   #readfile
-    read_csv = open(str(new_file_name()), "r")
+# @file_name: (string) the file 
+def read_file_data(file_name = ""): # readfile
+    if file_name == "":
+        file_name = str(new_file_name())
+    read_csv = open(file_name, "r")
     skip = read_csv.readline()
 
     for line in read_csv.readlines():
@@ -28,14 +31,17 @@ def read_file_data():                                                           
     pprint.pprint(data)
 
     read_csv.close()
+    return file_name
 
-def wwrite_file_data():                                                                                 #editfile
-    write_csv = open(str(new_file_name()), "a")
+def write_file_data(file_name = ""): # edit file
+    if file_name == "":
+        file_name = str(new_file_name())
+    write_csv = open(file_name, "a")
 
     #----headline----#
     #write_csv.write("alias,name,age,gender,position\n")
 
-#--------count exception handling--------#
+    #----count exception handling----#
     count = -1
     while count < 0:
         count = int(input("please input the count: "))
@@ -61,31 +67,45 @@ def wwrite_file_data():                                                         
 
         write_csv.write(",".join([alias, name, age, gender, position]) + "\n")
         write_csv.flush()
+
     write_csv.close()
 
+    return file_name
 
 
 ###--------main--------###
 
-i = 1
+i = -1
 pprint.pprint("1. new a file.")
 pprint.pprint("2. read a file.")
 pprint.pprint("3. edit a file.")
 pprint.pprint("0. exit.")
-while i in range(4):
-    i = int(input("please input the order you want to execute: "))
+
+file_name = ""
+
+while True:
+    try:
+        i = int(input("please input the order you want to execute: "))
+    except:
+        i = -1
+        print("[!!WARNING!!] please input a digit!")
+        continue
     while i != 1 and i != 2 and i != 3 and i != 0:
-        if i != 1 and i != 2 and i != 3 and i != 0:
-            print("[!!WARNING!!] please input the right order!")
+        print("[!!WARNING!!] please input the right order!")
+        try:
             i = int(input("please enter again: "))
+        except:
+            i = -1
+            continue
+
     if i == 1:
         new_file_name()
 
     if i == 2:
-        read_file_data()
+        file_name = read_file_data()
 
     if i == 3:
-        wwrite_file_data()
+        file_name = write_file_data()
 
         print("Would you want to check the data you type-in? ")
         print("YES / NO")
@@ -96,10 +116,11 @@ while i in range(4):
                 print("YES / NO")
                 j = input()
         if j == "YES":
-            read_file_data()
+            file_name = read_file_data(file_name)
         else:
             pprint.pprint("Congratulations! You have entered the data successfully. ")
 
     if i == 0:
         break
+
 print("Thank you for your use!")
